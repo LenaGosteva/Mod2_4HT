effa1672f086e2145c091c989ebcf7b6959df31e
LenaGosteva <114250252+LenaGosteva@users.noreply.github.com>
2022-11-11 22:02:51 +0300

    3

--- app/src/main/java/com/example/mod2_4htdiscr/MainActivity.java
+++ app/src/main/java/com/example/mod2_4htdiscr/MainActivity.java
@@ -47,26 +47,28 @@ public class MainActivity extends AppCompatActivity {
 //        return x;
 //    }
 
-    private void zero(EditText editText){
-        if (Double.parseDouble(editText.getText().toString()) ==0){
-            Toast.makeText(this, "Невозможно деление на ноль. Введите корректное значение", Toast.LENGTH_SHORT).show();
-        }
-    }
+
     public void click (View view){
         double a = get(editTextA);
         double b = get(editTextB);
         double c = get(editTextC);
-        zero(editTextA);
         String x = "";
-        double d = (double)(b*b - 4*a*c);
-        if (d<0) x += "Нет действительных корней";
-        else if (d == 0) x += String.format("%.2f",-b/(double)(2*a));
-        else if (d > 0){
-            double x1 = (double)((-b + Math.sqrt(d))/(2*a));
-            double x2 = (double)((-b - Math.sqrt(d))/(2*a));
-            x += String.format("%.2f",x1 +", "+ x2);
+
+        if (a == 0){
+            x += String.format("%.2f", -c/(double)b);
+            textViewX.setText(x);
         }
-        textViewX.setText(x);
-        Toast.makeText(this, a + "x^2 + " + b + "x + " + c + " = 0", Toast.LENGTH_LONG).show();
+        else{
+            double d = (double) (b * b - 4 * a * c);
+            if (d < 0) x += "Нет действительных корней";
+            else if (d == 0) x += String.format("%.2f", -b / (double) (2 * a));
+            else if (d > 0) {
+                double x1 = (double) ((-b + Math.sqrt(d)) / (2 * a));
+                double x2 = (double) ((-b - Math.sqrt(d)) / (2 * a));
+                x += String.format("%.2f", x1 + ", " + x2);
+            }
+            textViewX.setText(x);
+            Toast.makeText(this, a + "x^2 + " + b + "x + " + c + " = 0", Toast.LENGTH_LONG).show();
+        }
     }
 }
